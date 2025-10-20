d5a645ed499a3b80253cd2e1f973115b67de26ef
Paul Beard <paulbread88@gmail.com>
2025-10-20 21:17:11 +0100

    remove skip as now passable

--- tests/testthat/test-explain_iblm.R
+++ tests/testthat/test-explain_iblm.R
@@ -210,8 +210,6 @@ testthat::test_that("test explain completes when categorical only", {
 
 testthat::test_that("test explain completes when continuous only", {
 
-  testthat::skip("This is awaiting feedback from KG...")
-
   vars <- c("VehPower", "VehAge", "DrivAge", "BonusMalus", "Density", "ClaimRate")
 
   withr::with_seed(1, {
