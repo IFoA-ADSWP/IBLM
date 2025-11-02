ee971e399973fbf7e68915bdfaf0771e569f8551
Paul Beard <paulbread88@gmail.com>
2025-11-02 10:44:20 +0000

    wire "test against karol paper" to be independant of data object

--- tests/testthat/test-get_pinball_scores.R
+++ tests/testthat/test-get_pinball_scores.R
@@ -81,6 +81,33 @@ testthat::test_that("test against Karol original script", {
 testthat::test_that("test against Karol paper", {
 
 
+  # test takes too long for CRAN
+  testthat::skip_on_cran()
+
+
+  # ============================ Download data =====================
+
+  # download the correct version of freMTPL2freq dataset to complete the rec
+
+  commit <- "c49cbbb37235fc49616cac8ccac32e1491cdc619"  # <- use this commit
+
+  url <- paste0("https://github.com/dutangc/CASdatasets/raw/", commit, "/data/freMTPL2freq.rda")
+
+  temp <- tempfile()
+
+  download.file(url, temp)
+
+  load(temp)
+
+  freMTPL2freq <- freMTPL2freq |>
+    dplyr::mutate(
+      ClaimRate = ClaimNb / Exposure,
+      ClaimRate = pmin(ClaimRate, quantile(ClaimRate, 0.999)), # <-- kept in to help rec with original paper
+      VehAge = pmin(VehAge,50) # <-- kept in to help rec with original paper
+    ) |>
+    dplyr::select(-dplyr::all_of(c("IDpol", "Exposure", "ClaimNb")))
+
+
   # ============================ Input data =====================
 
   withr::with_seed(1, {
