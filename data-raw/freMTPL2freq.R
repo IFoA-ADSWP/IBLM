4298075269cca047939d73c353a0cf54337a0aa9
Paul Beard <paulbread88@gmail.com>
2025-11-02 16:47:02 +0000

    shrink data size and make out-the-box for functions

--- data-raw/freMTPL2freq.R
+++ data-raw/freMTPL2freq.R
@@ -1,7 +1,9 @@
 # code pulls the `freMTPL2freq` data object from GitHub and saves as compressed data object for our package
+library(magrittr)
 
 commit <- "master"  # <- use this commit for the latest `CASdatasets` variant
 commit <- "c49cbbb37235fc49616cac8ccac32e1491cdc619" # <- use this commit for the kaggle variant
+seed_no <- 9000
 
 url <- paste0("https://github.com/dutangc/CASdatasets/raw/", commit, "/data/freMTPL2freq.rda")
 
@@ -19,6 +21,9 @@ freMTPL2freq <- freMTPL2freq |>
   ) |>
   # turn any character fields into factors, should help keep package memory lower
   dplyr::mutate(dplyr::across(dplyr::where(is.character), function(field) factor(field))) |>
-  dplyr::select(-dplyr::all_of(c("IDpol", "Exposure", "ClaimNb")))
+  dplyr::select(-dplyr::all_of(c("IDpol", "Exposure", "ClaimNb"))) |>
+  dplyr::mutate(ClaimRate = round(ClaimRate)) |>
+  dplyr::slice_sample(n = 50000) %>%
+  withr::with_seed(seed_no, .)
 
-usethis::use_data(freMTPL2freq, overwrite = TRUE, compress = "xz")
+usethis::use_data(freMTPL2freq, overwrite = TRUE)
